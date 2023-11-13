28f8b9e009ae17070c5ea730f7e4d29ff28f0188
ssirrikh <13358109+Ssirrikh@users.noreply.github.com>
2023-11-13 00:43:46 -0800

    add resource scanner for music

---
+++ music/resources.js
@@ -0,0 +1,11 @@
+let resources = [
+	{ path: "playlist-00/Ever So Blue - Progress.mp3", composer: "Ever So Blue", title: "Progress" },
+	{ path: "playlist-00/Jakob Ahlbom - Distant Echo.mp3", composer: "Jakob Ahlbom", title: "Distant Echo" },
+	{ path: "playlist-00/Jakob Ahlbom - Endeavour.mp3", composer: "Jakob Ahlbom", title: "Endeavour" },
+	{ path: "playlist-00/Jakob Ahlbom - Pulsar.mp3", composer: "Jakob Ahlbom", title: "Pulsar" },
+	{ path: "playlist-00/Lama House - Cloaked Emotions.mp3", composer: "Lama House", title: "Cloaked Emotions" },
+	{ path: "playlist-00/Thomas Bergersen - Eyes Closing.mp3", composer: "Thomas Bergersen", title: "Eyes Closing" },
+	{ path: "playlist-00/Thomas Bergersen - Forever in My Dreams.mp3", composer: "Thomas Bergersen", title: "Forever in My Dreams" },
+	{ path: "playlist-00/Thomas Bergersen - Remember Me.mp3", composer: "Thomas Bergersen", title: "Remember Me" },
+	{ path: "playlist-00/Tony Anderson - Ariana.mp3", composer: "Tony Anderson", title: "Ariana" }
+];
